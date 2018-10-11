dc55959eda17cb7d21d40ec24ab689fba3ca3892
UbedaTHE1 <zxatzyt@gmail.com>
2018-10-11 11:17:22 +0200

    Add files via upload

--- Consola2/Consola2_3.java
+++ Consola2/Consola2_3.java
@@ -8,6 +8,8 @@ public class Consola2_3 {
     String producte, codi;
     long unitEstoc;
     double preu;
+    double pesetas;
+
 
     //Part de la introducció de dades del usuari
     Scanner dades = new Scanner(System.in);
@@ -30,14 +32,24 @@ public class Consola2_3 {
 
     codi = dades.next();
 
+    //conversió euros a pesetas
+    pesetas = (preu * 166);
 
-    //Part de la conversió en la capsa de signes
-    System.out.printf(" %n");
+    //Part de la conversió en el bloc de signes
+    //producte i codi
+    System.out.printf("%n+-------------------------------+%n");
+    System.out.printf("|%s%8s|",producte,"");
+    System.out.printf("%16s|",codi);
+    System.out.printf("%n+-------------------------------+%n");
 
-    System.out.printf("+-------------------------------+%n");
-
-    System.out.printf("" , producte);
-
+    //estoc, preu i pesetas
+    System.out.printf("|%14s|","Estoc:  ");
+    System.out.printf("%8s unitats|",unitEstoc);
+    //next line
+    System.out.printf("%n|%s%7s|","Import:","");
+    System.out.printf("%16s|","");
+    System.out.printf("%n|%10s €|",preu,"");
+    System.out.printf("%7s pessetes|%n",pesetas);
 
 
 
